f62df73d749d3e7a0b5880acd3f0dd22769b164a
Toolcats3276 <toolcatsrobotics@gmail.com>
2023-07-27 21:08:03 -0500

    test subsystem

---
+++ src/main/java/frc/robot/subsystems/testSubsystem.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+import frc.robot.Constants;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class testSubsystem extends SubsystemBase {
+
+
+}
